c38e9049196af4e9f2266160efc081c879c239dc
Elina Singh <elinasingh1997@gmail.com>
2024-01-24 08:54:43 +0530

    now we can delete items from list after click

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,17 @@ function App() {
     setInputValue("");
   }
 
+  function deleteItem(id) {
+    //console.log(id);
+    setItems((prevItems) => {
+      return prevItems.filter(
+        (item, index) => {
+          return index !== id;
+        }
+      )
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
@@ -33,8 +44,13 @@ function App() {
       </div>
       <div>
         <ul>
-        {items.map((toDoItem) => (
-          <ToDoItem text={toDoItem} />
+        {items.map((toDoItem, index) => (
+          <ToDoItem 
+          key={index}
+          id={index}
+          text={toDoItem}
+          onChecked={deleteItem}
+           />
         ) )}
         </ul>
       </div>
